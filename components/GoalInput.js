e56270e7ff9086db807a0875ac3c97df3d022724
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-07-01 10:26:14 -0300

    melhorado a estilização do modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -22,7 +22,14 @@ const GoalInput = (props) => {
           value={enteredGoalText}
           onChangeText={goalInputHandler}
         />
-        <Button title="Adicionar Meta" onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Adicionar Meta" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancelar" onPress={addGoalHandler} />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -33,18 +40,26 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: "60%",
-    marginRight: 8,
+    width: "100%",
     padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: "40%",
+    marginHorizontal: 8,
   },
 });
